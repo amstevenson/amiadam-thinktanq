1000e786d09f9281724aacc93aa8c33cc742388e
treacle74 <ptrelease@gmail.com>
2018-10-16 15:09:32 +0100

    new bash startup script and configuration

--- amiadam.sh
+++ amiadam.sh
@@ -1,1 +1,19 @@
-python3 ./manage.py runserver
+#!/bin/bash
+
+DIR=/home/dmdev/amiadam-thinktanq
+cd $DIR
+
+git pull
+#if [ -a venv ]; then
+#  source venv/bin/activate 
+#else
+#  /usr/bin/virtualenv venv
+#  source venv/bin/activate
+#fi
+
+source $DIR/venv/bin/activate
+pip3 install -r $DIR/requirements.txt 
+
+source $DIR/settings.conf
+
+python3 manage.py runserver
